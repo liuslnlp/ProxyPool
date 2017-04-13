cd41a4df74530df11b49da726476d38cb9722920
Doge <wisedoge@outlook.com>
2017-04-13 13:17:33 +0800

    Update run.py

--- run.py
+++ run.py
@@ -10,11 +10,11 @@ from proxypool.api import app
 from proxypool.schedule import Schedule
 from multiprocessing import Process
 
-def main():
+def cli():
     s = Schedule()
     s.run()
     app.run()
 
 if __name__ == '__main__':
-    main()
+    cli()
 
